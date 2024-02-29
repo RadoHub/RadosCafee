bd200c1349140c19258f70f15d83677c750cb919
Rado <46551472+RadoHub@users.noreply.github.com>
2024-02-29 12:55:08 +0100

    Add files via upload

---
+++ RadosCafee.Infrastructure/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using RadosCafee.Application.Interfaces.Repositories;
+using RadosCafee.Domain.Common;
+using RadosCafee.Domain.Common.Interfaces;
+using RadosCafee.Infrastructure.Services;
+
+namespace RadosCafee.Infrastructure.Extensions
+{
+    public static  class IServiceCollectionExtensions
+    {
+        public static void AddInfraStructureLayer (this IServiceCollection services)
+        {
+            services.AddServices();
+        }
+
+        private static void AddServices(this IServiceCollection services)
+        {
+            services.AddTransient<IMediator, Mediator>()
+                .AddTransient<IDomainEventDispatcher, DomainEventDispatcher>()
+                .AddTransient<IDateTimeService, DateTimeService>();
+        }
+    }
+}
---
+++ RadosCafee.Infrastructure/Services/DateTimeService.cs
@@ -0,0 +1,14 @@
+﻿using RadosCafee.Application.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadosCafee.Infrastructure.Services
+{
+    public class DateTimeService : IDateTimeService
+    {
+        public DateTime NotUtc =>  DateTime.UtcNow;
+    }
+}
