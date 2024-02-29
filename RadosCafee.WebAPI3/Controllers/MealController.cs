16ec63733720a69e79fab4bcfa82884c1c0d0ed8
Rado <46551472+RadoHub@users.noreply.github.com>
2024-02-29 12:59:49 +0100

    Add files via upload

---
+++ RadosCafee.WebAPI3/Controllers/MealController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RadosCafee.Application.Features.Meals.Commands;
+using RadosCafee.Application.Features.Meals.Queries.GetAllMeals;
+using RadosCafee.Shared.Concrete;
+
+namespace RadosCafee.WebAPI3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MealController : ControllerBase
+    {
+        private IMediator _mediator;
+        public MealController(IMediator mediator)
+        {
+            _mediator=mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Result<List<GetAllMealsDto>>>> GetMeals()
+        {
+            return await _mediator.Send(new GetAllMealsQuery());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Result<int>>> Create(CreateMealCommand command)
+        {
+            return await _mediator.Send(command);
+        }
+    }
+}
---
+++ RadosCafee.WebAPI3/Program.cs
@@ -0,0 +1,31 @@
+using RadosCafee.Application.Extensions;
+using RadosCafee.Infrastructure.Extensions;
+using RadosCafee.Persistence.Extensions;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddApplicationLayer();
+builder.Services.AddInfraStructureLayer();
+builder.Services.AddPersistanceLayer(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
